174ff6f44b9c7dd2b58db9c8fb0dc37c8f7f0d50
Jacek Pilch <jaaahooou@wp.pl>
2022-11-20 13:15:14 +0100

    user serializers

--- backend/mainApp/views.py
+++ backend/mainApp/views.py
@@ -3,10 +3,40 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, User,UserSerializer, UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .products import products
 
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+       data = super().validate(attrs)
+       serializer = UserSerializerWithToken(self.user).data 
+       for k,v in serializer.items():
+        data[k]= v
+
+
+       return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user
+
+    products= Product.objects.all()
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getRoutes(request):
