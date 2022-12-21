801dd5ff68e5a3a4b2a1edfd8388a6a5d5aa881c
Jacek Pilch <jaaahooou@wp.pl>
2022-12-21 08:33:26 +0100

    reset databese

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,6 +139,17 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'incredibleshop',
+#         'USER': 'postgres',
+#         'PASSWORD': '31537597Gg',
+#         'HOST' : 'localhost',
+#         'PORT':'5432',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
